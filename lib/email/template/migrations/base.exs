9320268cfa7faede697f90674fc50bff3f0a2a86
Lyle Mantooth <lyle@menteeth.us>
2023-05-11 13:28:50 -0400

    Need text column for large templates.

--- lib/email/template/migrations/base.exs
+++ lib/email/template/migrations/base.exs
@@ -6,7 +6,7 @@ defmodule Rivet.Email.Template.Migrations.Base do
     create table(:email_templates, primary_key: false) do
       add(:id, :uuid, primary_key: true)
       add(:name, :string)
-      add(:data, :string)
+      add(:data, :text)
       timestamps()
     end
   end
