67faa7325b3b293748c66f787ab0712b331e5f22
Brandon Gillespie <bjg@solv.com>
2023-09-15 09:42:27 -0600

    v1.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RivetEmail.MixProject do
   def project do
     [
       app: :rivet_email,
-      version: "1.3.0",
+      version: "1.4.0",
       package: package(),
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -58,10 +58,10 @@ defmodule RivetEmail.MixProject do
       {:html_sanitize_ex, "~> 1.4"},
       {:jason, "~> 1.0"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:rivet, "~> 2.1"},
+      {:rivet, "~> 2.2"},
       {:swoosh, "~> 1.9"},
       {:timex, "~> 3.6"},
-      {:transmogrify, "~> 1.1.0"}
+      {:transmogrify, "~> 2.0.2"}
     ]
   end
 
