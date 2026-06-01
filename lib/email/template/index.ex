5bde00f3ede7e6ed864882772b0a042015ff1888
Brandon Gillespie <brandon@cold.org>
2026-06-01 11:10:53 -0600

    .

--- lib/email/template/index.ex
+++ lib/email/template/index.ex
@@ -55,7 +55,7 @@ defmodule Rivet.Email.Template do
       end
 
       @impl Rivet.Email.Template
-      def sendto(targets, assigns, configs \\ @configs),
+      def template_send(targets, assigns, configs \\ @configs),
         do: Rivet.Email.mailer().sendto(targets, __MODULE__, merge_assigns(assigns), configs)
 
       defoverridable sendto: 2, sendto: 3
